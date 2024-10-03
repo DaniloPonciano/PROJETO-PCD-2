7af033d824e5abb0071f87ddce08ad0192c9d681
Danilo Ponciano <danilo_ponciano@estudante.sc.senai@gmail.com>
2024-10-02 21:36:08 -0300

    Criado a tela de Deficiencia

--- src/main/java/com/example/demo/Repository/DeficienciaRepository.java
+++ src/main/java/com/example/demo/Repository/DeficienciaRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.Model.Deficiencia;
@@ -7,4 +9,5 @@ import com.example.demo.Model.Deficiencia;
 @Repository
 public interface DeficienciaRepository extends JpaRepository<Deficiencia, Long> {
 
+    List<Deficiencia> findByAtivo(boolean ativo);
 }
